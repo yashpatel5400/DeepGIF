444f69428312a09d68e2b4feadfa9b9c2f149ea2
yashpatel5400 <yashpatel5400@gmail.com>
2016-12-27 17:22:55 -0500

    Simplifying code

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ __description__ = Global variable declarations (module-level)
 CONTENT_FEATURE_LAYER = 3 
 
 # gradiet ascent number of iterations (producing image)
-NUM_ITERATIONS = 5
+NUM_ITERATIONS = 10
 
 # gradient ascent step size
 STEP_SIZE = 1.0
