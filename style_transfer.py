18cc3ad2ec86ecd378a4e126b932981acf3596f4
yashpatel5400 <yashpatel5400@gmail.com>
2016-12-27 17:31:06 -0500

    Fixed bug

--- style_transfer.py
+++ style_transfer.py
@@ -120,7 +120,7 @@ def transform(content_features, content_weight, style_features, style_weights,
 	grad = lambda x : np.array(loss_grad[1:]).flatten().astype('float64')
 
 	for i in range(s.NUM_ITERATIONS):
-		input_img_data.reshape((1, WIDTH, HEIGHT, 3))
+		input_img_data = input_img_data.reshape((1, WIDTH, HEIGHT, 3))
 		loss_grad = iterate([input_img_data])
 		input_img_data, min_val, info = fmin_l_bfgs_b(loss, 
 			input_img_data.flatten(), fprime=grad, maxfun=20)
