2fb3cf12008e21471370d68c7166eb0125747ba6
yashpatel5400 <yashpatel5400@gmail.com>
2016-12-28 00:17:03 -0500

    Added video stylization

--- settings.py
+++ settings.py
@@ -15,17 +15,17 @@ STEP_SIZE = 1.0
 
 # ------------------------- Input Global Variables ---------------------------#
 # location where the content inputs are saved
-INPUT_CONTENT_DIR = './input/content/'
+INPUT_CONTENT_DIR = './input/content'
 
 # location where the style inputs are saved
-INPUT_STYLE_DIR = './input/style/'
+INPUT_STYLE_DIR = './input/style'
 
 # location where a video file is broke into its constituent frames
-INPUT_FRAME_DIR = './input/frames/'
+INPUT_FRAME_DIR = './input/frames'
 
 # ------------------------- Output Global Variables --------------------------#
 # location where analyzed/stylized video frames are saved
-OUTPUT_FRAME_DIR = './results/frames/'
+OUTPUT_FRAME_DIR = './results/frames'
 
 # location where the overall (final) style transfers are saved
-OUTPUT_FINAL_DIR = './results/final/'
+OUTPUT_FINAL_DIR = './results/final'
