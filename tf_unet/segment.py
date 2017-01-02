39349894a1871388890fc7a6d55f0417f955a4c3
yashpatel5400 <yashpatel5400@gmail.com>
2017-01-02 00:13:09 -0500

    Added UNet (branched) implemented: need to train

--- tf_unet/segment.py
+++ tf_unet/segment.py
@@ -13,10 +13,12 @@ ny = 572
 generator = image_gen.GrayScaleDataProvider(nx, ny, cnt=20)
 x_test, y_test = generator(4)
 
+"""
 fig, ax = plt.subplots(1,2, sharey=True, figsize=(8,4))
 ax[0].imshow(x_test[0,...,0], aspect="auto")
 ax[1].imshow(y_test[0,...,1], aspect="auto")
 plt.show()
+"""
 
 net = unet.Unet(channels=generator.channels, n_class=generator.n_class, layers=3, features_root=16)
 trainer = unet.Trainer(net, optimizer="momentum", opt_kwargs=dict(momentum=0.2))
