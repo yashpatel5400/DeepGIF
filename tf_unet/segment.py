df9f81e4ac0a29cc0eb4d2a629862d498859b78b
yashpatel5400 <yashpatel5400@gmail.com>
2017-01-02 00:51:29 -0500

    Modified training model

--- tf_unet/segment.py
+++ tf_unet/segment.py
@@ -22,4 +22,18 @@ plt.show()
 
 net = unet.Unet(channels=generator.channels, n_class=generator.n_class, layers=3, features_root=16)
 trainer = unet.Trainer(net, optimizer="momentum", opt_kwargs=dict(momentum=0.2))
-path = trainer.train(generator, "./unet_trained", training_iters=20, epochs=100, display_step=2)
+# path = trainer.train(generator, "./unet_trained", training_iters=20, epochs=100, display_step=2)
+
+prediction = net.predict("./unet_trained/model.cpkt", x_test)
+fig, ax = plt.subplots(2, 3, sharex=True, sharey=True, figsize=(12,8))
+for i in range(ax.shape[0]):
+    ax[i, 0].imshow(x_test[i,...,0], aspect="auto")
+    ax[i, 1].imshow(y_test[i,...,1], aspect="auto")
+    mask = prediction[i,...,1] > 0.9
+    ax[i, 2].imshow(mask, aspect="auto")
+
+ax[0, 0].set_title("Input")
+ax[0, 1].set_title("Ground truth")
+ax[0, 2].set_title("Prediction")
+fig.tight_layout()
+fig.savefig("./toy_problem.png")
