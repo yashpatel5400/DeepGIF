e3d40e86000919a91dc8960c85f6224fbdea7553
Yash Patel <yashpatel5400@gmail.com>
2017-01-12 22:47:58 -0500

    Changed download code

--- processing/segmentation/preprocess.py
+++ processing/segmentation/preprocess.py
@@ -47,26 +47,7 @@ def download_model(cache_dir=s.MODEL_CACHE):
     :return: None
     """
     file_name = s.MODEL_SITE.split('/')[-1]
-    u = urllib2.urlopen(s.MODEL_SITE)
-    f = open(file_name, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print("Downloading: {} Bytes: {}".format(file_name, file_size))
-
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
-
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8) * (len(status)+1)
-        print(status)
-    
-    f.close()
+    urllib.urlretrieve(s.MODEL_SITE, file_name)
     shutil.move("./{}".format(file_name), "{}{}".format(cache_dir, file_name))
 
 def add_padding(im):
